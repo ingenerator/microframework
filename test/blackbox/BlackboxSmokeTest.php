5f3001de8ee2d5faa1930bf0432beadeb38cf5f3
acoulton <andrew@ingenerator.com>
2024-07-17 13:27:52 +0100

    Refactor commonality from initial smoketest

--- test/blackbox/BlackboxSmokeTest.php
+++ test/blackbox/BlackboxSmokeTest.php
@@ -5,14 +5,15 @@ namespace test\blackbox;
 use GuzzleHttp\Client;
 use PHPUnit\Framework\TestCase;
 
-class BlackboxSmokeTest extends TestCase
+class BlackboxSmokeTest extends BaseBlackboxTestCase
 {
     public function test_test_subject_responds_from_default_handler()
     {
-        $client = new Client();
-        $response = $client->get('http://test_subject/');
-        $this->assertSame(200, $response->getStatusCode());
-        $this->assertSame('Blackbox is alive', $response->getBody()->getContents());
+        $this->assertResponseMatches(
+            200,
+            'Blackbox is alive',
+            $this->guzzle->get('/'),
+        );
     }
 
 }
